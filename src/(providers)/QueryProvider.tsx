dc92fdb94f757d04d4019b17f7905888b9e5b18d
hayanLee <hayanlee.dev@gmail.com>
2024-07-04 00:15:45 +0900

    add : react query 연결
    - 기본 레이아웃 설정
    - PokemonList, PokemonCard

---
+++ src/(providers)/QueryProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+function QueryProvider({ children }: PropsWithChildren) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    );
+}
+
+export default QueryProvider;
---
+++ src/components/PokemonList/PokemonList.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+function PokemonList() {
+    return <div>PokemonList</div>;
+}
+
+export default PokemonList;
