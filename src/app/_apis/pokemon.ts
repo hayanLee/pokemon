4d58620230c22204d79a60246849957b36352dfe
hayanLee <hayanlee.dev@gmail.com>
2024-07-06 13:46:07 +0900

    fix : 도메인 주소 변경

--- src/app/_apis/pokemon.ts
+++ src/app/_apis/pokemon.ts
@@ -1,8 +1,11 @@
 import { Pokemon } from '@/types/pokemon.type';
 import axios from 'axios';
 
+const LOCAL_URL = 'http://localhost:3000';
+const DEPLOY_URL = 'https://pokemon-murex-one.vercel.app';
+
 export const getPokemon = async (id: string): Promise<Pokemon> => {
-    const apiURL = 'http://localhost:3000';
+    const apiURL = DEPLOY_URL;
     const res = await axios.get<Pokemon>(`${apiURL}/api/pokemons/${id}`);
     return res.data;
 };
